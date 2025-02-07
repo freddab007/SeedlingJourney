a3ab1c11f137fd23bf96669c690593ce306bfaf2
freddab007 <erwann.perrin.i2d9@gmail.com>
2025-02-07 17:22:06 +0100

    Add tileData on tile for tile Harvestable or not

---
+++ Assets/Script/TileData/TileData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+[CreateAssetMenu]
+public class TileData : ScriptableObject
+{
+    public TileBase[] _tileBase;
+
+    public bool havertable;
+}
